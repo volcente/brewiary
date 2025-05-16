665a6b346be6988ae7d366c09b1c18ef93f60244
volcente <elert.bartosz@gmail.com>
2025-05-16 22:42:48 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid min-h-screen w-full p-8 place-content-center">
+      <h1 className="text-9xl font-semibold text-indigo-400">Hello World</h1>
+    </div>
+  );
+}
