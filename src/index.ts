0eabba076402d814427ff96df5a7b46dca4dbe1b
Bartosz Elert <elert.bartosz@gmail.com>
2025-09-07 19:13:24 +0200

    added drizzle-orm

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { config } from "dotenv";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import { usersTable } from "./db/schema";
+
+config({ path: ".env" });
+config({ path: ".env.local", override: true });
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log("New user created!");
+
+  const users = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users);
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log("User info updated!");
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log("User deleted!");
+  return;
+}
+
+main();
