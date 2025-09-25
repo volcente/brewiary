bfcb0028ff78fd8e31001ab8d43733036c2bd703
Bartosz Elert <elert.bartosz@gmail.com>
2025-09-25 19:06:24 +0200

    added list of ignored files and directories for eslint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 	schema: './src/db/schema.ts',
 	dialect: 'postgresql',
 	dbCredentials: {
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		url: process.env.DATABASE_URL!,
 	},
 });
