401aeebde4fc532992f90a2dd6859691bfcd8965
Bartosz Elert <elert.bartosz@gmail.com>
2025-09-29 21:13:53 +0200

    added shadcn with next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './styles/globals.css';
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 
+import { ThemeProvider } from '~/components/theme-provider/theme-provider';
+
 import { commitMono, spaceGrotesk } from './styles/fonts';
 
 export const metadata: Metadata = {
@@ -19,9 +21,18 @@ export default function RootLayout({ children }: PropsWithChildren) {
 		<html
 			className={`${commitMono.variable} ${spaceGrotesk.variable}`}
 			lang='en'
+			suppressHydrationWarning
 		>
-			<body className='bg-stone-200 font-sans text-stone-900 antialiased dark:bg-stone-900 dark:text-stone-200'>
-				{children}
+			<body className='font-sans antialiased'>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					disableTransitionOnChange
+					enableColorScheme
+					enableSystem
+				>
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
